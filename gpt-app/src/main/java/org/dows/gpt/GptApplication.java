3b78a18748d4a2b92f77ee76348d75cab96dee6c
lait <lait.zhang@gmail.com>
2025-08-03 17:45:52 +0800

    feat(gpt-app): 添加 Tencent OSS 支持
    - 在 GptApplication 中添加了对 rade 和 oss 包的扫描
    - 在 pom.xml 中添加了 oss-tencent 依赖

--- gpt-app/src/main/java/org/dows/gpt/GptApplication.java
+++ gpt-app/src/main/java/org/dows/gpt/GptApplication.java
@@ -16,7 +16,7 @@ import java.util.List;
 @EnableAutoTable // 开启自动建表
 @EnableAsync // 开启异步处理
 @EnableCaching // 开启缓存
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"org.dows.gpt", "org.dows.rade","org.dows.oss"})
 //@EnableFeignClients() //开启Feign客户端
 //@EnableFeignClients(basePackages={"org.dows.uim.feign","org.dows.uim.api"})
 public class GptApplication {
